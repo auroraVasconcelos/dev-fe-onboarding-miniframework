9c6467ea8f4d3ea7de20e73369faf7932292fbac
AuroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2021-07-05 18:15:27 +0100

    debug

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,29 @@
 import {animatedScrollTo} from './es6-scroll-to.js';
 
-document.querySelector('.title').innerHTML = 'Great success!'
-document.querySelector('.text').innerHTML = 'Javascript is working!'
+// document.querySelector('.title').innerHTML = 'Great success!'
+// document.querySelector('.text').innerHTML = 'Javascript is working!'
+
+let once = false;
+const contacts = Array.from(document.querySelectorAll('.banner__btn'));
+
+const test = () => {
+    console.log('click');
+}
+
+contacts.forEach((button) => {
+    button.addEventListener('click', test());
+});
+
+const show = () => {
+
+    if(once) {
+        once = false;
+        contacts.style.display = 'none';
+    }
+    else if(!once) {
+        once = true;
+        contacts.style.display = 'block';
+    }
+}
 
 animatedScrollTo(500);
