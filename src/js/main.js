6a4aa33d5720cdc2dbfdc6ade62670ff04f8f671
AuroraVasconcelos <aurora.vasconcelos@virtual-identity.com>
2021-07-30 10:56:02 +0100

    Fetch content from API

--- src/js/main.js
+++ src/js/main.js
@@ -1,5 +1,6 @@
 // //get all buttons named button__btn from the page
 // const counter = Array.from(document.querySelectorAll('.banner__btn'));
+
 // const toggleContacts = (event) => {
 //     //define clicked button
 //     const buttonClicked = event.target;
@@ -28,24 +29,39 @@ const buttons = Array.from(document.querySelectorAll('.banner__btn'));
 const allContacts = Array.from(document.querySelectorAll('.contacts'));
 const contactsVisibleClass = 'contacts--visible';
 
-allContacts.forEach(contact => { contact.addEventListener('toggleContactsEvent', function(toggleEvent) {
+const activeToggle = function(clickEvent){
+    const toggleContactsEvent = new CustomEvent('toggleContactsEvent', {
+        detail: {
+            buttonClicked: clickEvent.target.closest('.container').querySelector('.contacts'),
+        }
+    });
 
+    allContacts.forEach(contact => contact.dispatchEvent(toggleContactsEvent));
+}
+
+const toggleContactsVisibility = function(toggleEvent) {
     const activeContact = toggleEvent.detail.buttonClicked;
+
     if(activeContact.classList.contains(contactsVisibleClass)) {
         return activeContact.classList.remove(contactsVisibleClass);
     }
+
     allContacts.forEach(contact => {contact.classList.remove(contactsVisibleClass)});
     activeContact.classList.add(contactsVisibleClass);
-    
-    })
-});
+}
 
-buttons.forEach(button => button.addEventListener('click', function(clickEvent){
-    const contactToShow = clickEvent.target.closest('.container').querySelector('.contacts');
-    const toggleContacts = new CustomEvent('toggleContactsEvent', {
-        detail: {
-            buttonClicked: contactToShow
-        }
-    });
-    allContacts.forEach(contact => contact.dispatchEvent(toggleContacts));
-}));
+allContacts.forEach(contact => contact.addEventListener('toggleContactsEvent', toggleContactsVisibility));
+buttons.forEach(button => button.addEventListener('click', activeToggle));
+
+// //DELEGATION EVENT
+// const buttons = Array.from(document.querySelectorAll('.banner__btn'));
+
+// const toggleContacts = new Event = () => {
+//     console.log(document.activeElement);
+// }
+
+// document.addEventListener('toggleContactsEvent');
+
+// buttons.forEach(button => button.addEventListener('click', toggleContacts));
+
+// // o click e no body ou no main
